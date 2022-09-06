961f7611180c5b4bdb3459831fc8b429ba06c106
danieljedrasiewicz <daniel.jedrasiewicz@eurocall.pl>
2022-09-06 13:48:57 +0200

    :sparkles: add products category part 2

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -14,7 +14,7 @@
                             @csrf
                             @method('PUT')
                             <div class="row mb-3">
-                                <label for="name" class="col-md-4 col-form-label text-md-end">Nazwa</label>
+                                <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('shop.product.fields.name') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="name" maxlength="500" type="text"
@@ -30,7 +30,7 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="description" class="col-md-4 col-form-label text-md-end">Opis</label>
+                                <label for="description" class="col-md-4 col-form-label text-md-end">{{ __('shop.product.fields.description') }}</label>
 
                                 <div class="col-md-6">
                                     <textarea id="description" maxlength="1500"
@@ -47,7 +47,7 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="amount" class="col-md-4 col-form-label text-md-end">Ilość</label>
+                                <label for="amount" class="col-md-4 col-form-label text-md-end">{{ __('shop.product.fields.amount') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="amount" type="number"
@@ -63,7 +63,7 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="price" class="col-md-4 col-form-label text-md-end">Cena</label>
+                                <label for="price" class="col-md-4 col-form-label text-md-end">{{ __('shop.product.fields.price') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="price" type="number" step="0.01" min="0"
@@ -79,7 +79,26 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="image" class="col-md-4 col-form-label text-md-end">Grafika</label>
+                                <label for="category" class="col-md-4 col-form-label text-md-end">{{ __('shop.product.fields.category') }}</label>
+
+                                <div class="col-md-6">
+                                    <select class="form-select @error('price') is-invalid @enderror" name="category_id">
+                                        <option value="" selected>Brak</option>
+                                        @foreach($categories as $category)
+                                            <option value="{{ $category->id }}" @if($product->isSelectedCategory($category->id)) selected @endif>{{ $category->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    @error('price')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+
+                            <div class="row mb-3">
+                                <label for="image" class="col-md-4 col-form-label text-md-end">{{ __('shop.product.fields.image') }}</label>
                                 <div class="col-md-6">
                                     <input id="image" type="file" step="0.01" min="0"
                                            class="form-control @error('image') is-invalid @enderror" name="image">
@@ -103,7 +122,7 @@
                             <div class="row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
-                                        Zapisz
+                                        {{ __('shop.button.save') }}
                                     </button>
                                 </div>
                             </div>
